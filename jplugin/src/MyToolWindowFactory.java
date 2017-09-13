920e02332a74c9a87cbd3d16c088f034088b658b
AwayQuEM <awayquym@gmail.com>
2017-09-13 19:07:08 +0800

    window factory
    Signed-off-by: AwayQuEM <awayquym@gmail.com>

--- jplugin/src/MyToolWindowFactory.java
+++ jplugin/src/MyToolWindowFactory.java
@@ -1,11 +1,12 @@
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 
 public class MyToolWindowFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-
+        PsiFile
     }
 }
