2eb461ddb914f99a9aa772c8ff16efecae961634
AwayQuEM <awayquym@gmail.com>
2018-04-20 19:38:41 +0800

    cross domain
    Signed-off-by: AwayQuEM <awayquym@gmail.com>

--- lan/idlerest/src/main/java/com/away1994/idlerest/Application.java
+++ lan/idlerest/src/main/java/com/away1994/idlerest/Application.java
@@ -2,11 +2,28 @@ package com.away1994.idlerest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        /**
+         * ref: https://spring.io/guides/gs/rest-service-cors/
+         */
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/github").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
