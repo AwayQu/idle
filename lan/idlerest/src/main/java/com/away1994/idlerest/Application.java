367332fb95ad2bc69cbb2471ee7e710bffb3ad38
AwayQuEM <awayquym@gmail.com>
2018-05-10 00:36:08 +0800

    deploy branch
    Signed-off-by: AwayQuEM <awayquym@gmail.com>

--- lan/idlerest/src/main/java/com/away1994/idlerest/Application.java
+++ lan/idlerest/src/main/java/com/away1994/idlerest/Application.java
@@ -2,6 +2,8 @@ package com.away1994.idlerest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,9 +12,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import java.util.ArrayList;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 
@@ -38,3 +45,34 @@ public class Application {
         };
     }
 }
+
+
+//@SpringBootApplication
+//public class Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        /**
+//         * ref: https://spring.io/guides/gs/rest-service-cors/
+//         */
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                ArrayList<String> arrayList = new ArrayList<>();
+//                arrayList.add("/github");
+//                arrayList.add("/github/diagram/files");
+//                arrayList.add("/github/fileTree");
+//                arrayList.add("/github/project");
+//
+//                for (String url : arrayList) {
+//                    registry.addMapping(url).allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000");
+//                    registry.addMapping(url).allowCredentials(true); // cookie upload permission
+//                }
+//            }
+//        };
+//    }
+//}
