c8eb1ebf100c309d212912672c27ac910e0f20f4
AwayQuEM <awayquym@gmail.com>
2018-05-25 19:38:09 +0800

    refactor
    Signed-off-by: AwayQuEM <awayquym@gmail.com>

---
+++ lan/idlerest/src/main/java/com/away1994/idlerest/websocket/WebSocketConfig.java
@@ -0,0 +1,12 @@
+package com.away1994.idlerest.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+}
