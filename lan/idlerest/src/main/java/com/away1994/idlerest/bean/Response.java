671714718253906fbdfe5a8ed1fdc905f66bb73f
AwayQuEM <awayquym@gmail.com>
2018-05-26 18:48:30 +0800

    add loupan Signed-off-by: AwayQuEM <awayquym@gmail.com>
    Signed-off-by: AwayQuEM <awayquym@gmail.com>

--- lan/idlerest/src/main/java/com/away1994/idlerest/bean/Response.java
+++ lan/idlerest/src/main/java/com/away1994/idlerest/bean/Response.java
@@ -7,10 +7,17 @@ public class Response {
     public final static int CODE_FAIL = 400;
     private int code;
     private String msg;
+    private Object result;
 
     public Response(int code, String msg) {
         this.code = code;
         this.msg = msg;
+    }
+
+    public Response(int code, String msg, Object result) {
+        this.code = code;
+        this.msg = msg;
+        this.result = result;
     }
 
     public int getCode() {
@@ -27,5 +34,13 @@ public class Response {
 
     public void setMsg(String msg) {
         this.msg = msg;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
     }
 }
